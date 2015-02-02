ac6d797b8d9c1da9ffddbbcfab809d20d2bc0ed8
Youssef <youssef.emad.attia@gmail.com>
2015-02-02 13:51:25 +0200

    Issue #6 , edit indentation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,9 @@
 class Student < ActiveRecord::Base
 	before_save :ensure_authentication_token
  
- 	 devise :database_authenticatable, :token_authenticatable, :registerable,
+ 	devise :database_authenticatable, :token_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
 	has_and_belongs_to_many :groups
 	has_many :student_result_quizzes
 	has_many :quizzes, :through => :student_result_quizzes
