e7122800a237bfd4ebab07a35f3737ea93d887ee
Youssef <youssef.emad.attia@gmail.com>
2015-02-02 13:33:07 +0200

    Issue #6 , add devise token authenticable attribute to student and instructor models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :token_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_and_belongs_to_many :groups
 	has_many :student_result_quizzes
