a705567ca1c7a2cf7e187ed74ce30b237e7c0a3b
Youssef <youssef.emad.attia@gmail.com>
2015-02-02 13:31:18 +0200

    Issue #6 , add devise to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_and_belongs_to_many :groups
 	has_many :student_result_quizzes
 	has_many :quizzes, :through => :student_result_quizzes
