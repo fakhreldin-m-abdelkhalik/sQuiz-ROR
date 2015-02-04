8d00a8dac70484f5f5fe26042b6704a28bed96ae
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2015-02-04 11:38:45 +0200

    Issue #8,Add failing test to edit_question method

--- test/integration/editing_questions_test.rb
+++ test/integration/editing_questions_test.rb
@@ -1,6 +1,6 @@
 class EditingQuestionsTest < ActionDispatch::IntegrationTest	
 	setup {@question = Question.create(text: "Question Text Example", mark: 2, choices:["a","b","c","d"], right_answer: "a")}	
-	test 'successes to add question to a quiz' do		
+	test 'successes to edit question in a quiz' do		
  		patch "api/quizzes/editquestion/#{@question.id}",
  		{ question:	
 	 	{ text: "Question Text Example edited", mark: 3, choices:["d","c","b","a"], right_answer: "d" }	
@@ -10,5 +10,15 @@ class EditingQuestionsTest < ActionDispatch::IntegrationTest
 		assert_equal 200, response.status	
 		assert_equal Question.find(@question.id).choices, ["d","c","b","a"]
 		assert_equal question_response[:success], true
+	end			
+	test 'fails to edit a question in a quiz' do		
+ 		patch "api/quizzes/editquestion/#{@question.id}",
+ 		{ question:	
+	 	{ text:"", mark: 3, choices:["d","c","b","a"], right_answer: "d" }	
+		}.to_json,
+ 		{ 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s }		
+		question_response = json(response.body)
+		assert_equal 422, response.status	
+		assert_equal question_response[:success], false
 	end			
 end
