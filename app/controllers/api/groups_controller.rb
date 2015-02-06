56fc542aecb32deac1d9e60aab2a081fe655fb47
Youssef <youssef.emad.attia@gmail.com>
2015-02-06 08:03:52 +0200

    Issue #13 , modify groups controller to use current_instructor

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -4,11 +4,10 @@ class Api::GroupsController < Devise::RegistrationsController
 
   def add
 
-    instructor = Instructor.find_by_authentication_token(instructor_params)
     student = Student.find(student_params)
     group = Group.find(group_params)
 
-    if(group_owner_is_correct(instructor,group))
+    if(group.instructor == current_isntructor)
 
       group.students << student
       render status: 200,
@@ -19,7 +18,7 @@ class Api::GroupsController < Devise::RegistrationsController
     else
       render status: :unprocessable_entity,
              json: { success: false,
-                        info: {instructor: instructor.errors , student: student.errors , group: group.errors},
+                        info: "Instructor is noy authorized to add students from this group",
                         data: {} }
 
     end                    
@@ -28,22 +27,21 @@ class Api::GroupsController < Devise::RegistrationsController
 
   def remove
 
-    instructor = Instructor.find_by_authentication_token(instructor_params)
     student = Student.find(student_params)
     group = Group.find(group_params)
 
-    if(group_owner_is_correct(instructor,group))
+    if(group.instructor == current_isntructor)
 
       group.students.delete(student)
       render status: 200,
               json: { success: true,
                       info: "Removed",
-                      data: { instructor: instructor,
+                      data: { instructor: current_isntructor,
                               student: student , group: group} }
     else
       render status: :unprocessable_entity,
              json: { success: false,
-                        info: {instructor: instructor.errors , student: student.errors , group: group.errors},
+                        info: "Instructor is noy authorized to remove students from this group",
                         data: {} }
 
     end                    
@@ -52,25 +50,12 @@ class Api::GroupsController < Devise::RegistrationsController
 
   private
 
-  def instructor_params
-    params.require(:instructor).permit(:authentication_token)
-  end
-
   def student_params
     params.require(:student).permit(:id)
   end
 
   def group_params
     params.require(:group).permit(:id)
-  end
-
-  def group_owner_is_correct( applicant, group)
-
-    if(group.instructor == applicant)
-      true
-    else
-      false
-    end
   end
  
 end
