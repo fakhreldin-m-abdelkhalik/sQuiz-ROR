45d5d97099dca25681cfb7e7eb8c8be42cb29570
Youssef <youssef.emad.attia@gmail.com>
2015-02-06 07:59:55 +0200

    Issue #23 , comment user authentication checks from application controller as each controller needs a specific check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-    acts_as_token_authentication_handler_for Student
-    acts_as_token_authentication_handler_for Instructor
+  
+    # acts_as_token_authentication_handler_for Student
+    # acts_as_token_authentication_handler_for Instructor
 end
