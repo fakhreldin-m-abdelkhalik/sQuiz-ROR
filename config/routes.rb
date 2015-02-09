5ee3e19aa1e7b0e36656dfea20e18c98455f941c
Youssef <youssef.emad.attia@gmail.com>
2015-02-09 15:25:52 +0200

    Issue #7 , edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,20 +23,8 @@ Rails.application.routes.draw do
 
     post 'signin', to: 'sessions#instructor_create'
 
-    delete '/student/signout', to: 'signout#student_destroy'
+    delete 'student/signout', to: 'signout#student_destroy'
     delete 'instructor/signout', to: 'signout#instructor_destroy'
-
-    # devise_scope :student do
-    #   post 'students/signup' => 'students_registrations#create', as: 'student_register'
-    #   post 'students/signin' => 'students_create#create', as: 'student_login'
-    #   delete 'students/signout' => 'students_destroy#destroy', as: 'student_logout'
-    # end
-
-    # devise_scope :instructor do
-    #   post 'instructors/signup' => 'instructors_registrations#create', as: 'instructor_register'
-    #   post 'instructors/signin' => 'instructors_create#create', as: 'instructor_login'
-    #   delete 'instructors/signout' => 'instructors_destroy#destroy', as: 'instructor_logout'
-    # end
     
     post 'groups/student/add' => 'groups#add'
     post 'groups/student/remove' => 'groups#remove'
