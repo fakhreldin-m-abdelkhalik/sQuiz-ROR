55061a1d7d53040c6d8281ed22b4e746847ed194
Youssef <youssef.emad.attia@gmail.com>
2015-02-09 15:25:03 +0200

    Issue #7 , remove 2 actions and include them in the first one

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,29 +1,36 @@
-class Api::SessionsController < Devise::SessionsController
+class Api::SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
  
   respond_to :json
 
   def instructor_create
-    Instructor.authenticate!(email,password, recall: "#{controller_path}#student_create")
-    render status: 200,
+
+    instructor =Instructor.find_for_database_authentication(:email => params[:email])
+    if instructor && instructor.valid_password?(params[:password])
+        sign_in(instructor)
+        render status: 200,
            json: { success: true,
                       info: "Logged in",
                       data: { auth_token: current_instructor.authentication_token } }
-  end
+    else
 
-  def student_create
-    Student.authenticate!(email,password, recall: "#{controller_path}#failure")
-    render status: 200,
+      student = Student.find_for_database_authentication(:email => params[:email])
+      if student && student.valid_password?(params[:password])
+        sign_in(student)
+        render status: 200,
            json: { success: true,
                       info: "Logged in",
-                      data: { auth_token: current_instructor.authentication_token } }
-  end
-
-  def failure
-    render :status => 401,
+                      data: { auth_token: current_student.authentication_token } }
+      else
+        render :status => 401,
            :json => { :success => false,
                       :info => "Login Failed",
                       :data => {} }
+      end
+
+    end
+    
   end
+
 end
