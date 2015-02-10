0d6de6f489848d66011810411c7874d07ab366dd
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2015-02-10 21:53:59 +0200

    Issue #16,Add test for creating quiz

--- spec/controllers/quizzes_controller_spec.rb
+++ spec/controllers/quizzes_controller_spec.rb
@@ -94,4 +94,19 @@ RSpec.describe Api::QuizzesController, :type => :controller do
 	     	expect(quiz_response[:info]).to eql("Quiz is not found")	
 	    end
     end
+
+    describe "create method succeeding" do
+	    it "creates a new quiz for the current instructor" do
+	    	sign_in @instructor
+	    	post :create, quiz: { name:"quiz1", subject:"maths", duration: 10, no_of_MCQ: 5, no_of_rearrangeQ: 5 }
+	    	quiz_response = json(response.body)
+	    	expect(response.status).to eq(201)
+	     	expect(quiz_response[:success]).to eql(true)
+	     	expect(quiz_response[:data][:quiz][:name]).to eql("quiz1")	
+	     	expect(quiz_response[:data][:quiz][:subject]).to eql("maths")	
+	     	expect(quiz_response[:data][:quiz][:duration]).to eql(10)	
+	     	expect(quiz_response[:data][:quiz][:no_of_MCQ]).to eql(5)
+	     	expect(quiz_response[:data][:quiz][:no_of_rearrangeQ]).to eql(5)	
+	    end
+    end
 end
