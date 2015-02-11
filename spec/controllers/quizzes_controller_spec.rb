0a49a5efd7139f46fde039b044112113a59fccd9
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2015-02-11 18:11:57 +0200

    Issue #16,Add test for editing question succeding

--- spec/controllers/quizzes_controller_spec.rb
+++ spec/controllers/quizzes_controller_spec.rb
@@ -25,8 +25,7 @@ RSpec.describe Api::QuizzesController, :type => :controller do
 	    	sign_in @student
 	    	@quiz = create(:quiz)
 	    	@quiz.update(expiry_date: DateTime.now - 1.hour)
-	    	@question = create(:question)
-	    	@quiz.questions << @question
+	    	assign_create_question
 	    	@student.quizzes << @quiz
 	    	get :student_show, id: @quiz.id
 	    	quiz_response = json(response.body)
@@ -41,8 +40,7 @@ RSpec.describe Api::QuizzesController, :type => :controller do
 	    it "returns a specific quiz and its questions" do
 	    	sign_in @student
 	    	@quiz = create(:quiz)
-	    	@question = create(:question)
-	    	@quiz.questions << @question
+	    	assign_create_question
 	    	@student.quizzes << @quiz
 	    	get :student_show, id: 25
 	    	quiz_response = json(response.body)
@@ -69,8 +67,7 @@ RSpec.describe Api::QuizzesController, :type => :controller do
 	    it "returns a specific quiz and its questions" do
 	    	sign_in @instructor
 	    	@quiz = create(:quiz)
-	    	@question = create(:question)
-	    	@quiz.questions << @question
+	    	assign_create_question
 	    	@instructor.quizzes << @quiz
 	    	get :instructor_show, id: @quiz.id
 	    	quiz_response = json(response.body)
@@ -85,8 +82,7 @@ RSpec.describe Api::QuizzesController, :type => :controller do
 	    it "returns a specific quiz and its questions" do
 	    	sign_in @instructor
 	    	@quiz = create(:quiz)
-	    	@question = create(:question)
-	    	@quiz.questions << @question
+	    	assign_create_question
 	    	@instructor.quizzes << @quiz
 	    	get :instructor_show, id: 25
 	    	quiz_response = json(response.body)
@@ -124,8 +120,7 @@ RSpec.describe Api::QuizzesController, :type => :controller do
 	    it "deletes a quiz from the instructor quizzes" do
 	    	sign_in @instructor
 	    	@quiz = create(:quiz)
-	    	@question = create(:question)
-	    	@quiz.questions << @question
+	    	assign_create_question
 	    	@instructor.quizzes << @quiz
 	    	delete :destroy, id: @quiz.id
 	    	quiz_response = json(response.body)
@@ -137,12 +132,11 @@ RSpec.describe Api::QuizzesController, :type => :controller do
 	    end
     end
 
-    describe "delete method failing" do
+    describe "delete quiz method failing" do
 	    it "fails to delete a quiz from the instructor quizzes" do
 	    	sign_in @instructor
 	    	@quiz = create(:quiz)
-	    	@question = create(:question)
-	    	@quiz.questions << @question
+	    	assign_create_question
 	    	@instructor.quizzes << @quiz
 	    	delete :destroy, id: 2
 	    	quiz_response = json(response.body)
@@ -157,11 +151,8 @@ RSpec.describe Api::QuizzesController, :type => :controller do
     describe "publish method succeeding" do
 	    it "publishes a quiz from the instructor quizzes to a future date and time" do
 	    	sign_in @instructor
-	    	@quiz = create(:quiz)
-	    	@group = create(:group)
+			assign_create_quiz_group
 	    	@group.students << @student
-	    	@instructor.quizzes << @quiz
-	    	@instructor.groups << @group
 	    	post :publish, { id: @quiz.id, group_id: @group.id, expiry_date: (DateTime.now + 1.day).to_s}
 	    	quiz_response = json(response.body)
 	    	expect(response.status).to eq(202)
@@ -174,11 +165,8 @@ RSpec.describe Api::QuizzesController, :type => :controller do
     describe "publish method failing" do
 	    it "fails to publish a quiz from the instructor quizzes because the instructor specifies past time" do
 	    	sign_in @instructor
-	    	@quiz = create(:quiz)
-	    	@group = create(:group)
+	    	assign_create_quiz_group
 	    	@group.students << @student
-	    	@instructor.quizzes << @quiz
-	    	@instructor.groups << @group
 	    	post :publish, { id: @quiz.id, group_id: @group.id, expiry_date: (DateTime.now - 1.day).to_s}
 	    	quiz_response = json(response.body)
 	    	expect(response.status).to eq(422)
@@ -191,11 +179,8 @@ RSpec.describe Api::QuizzesController, :type => :controller do
     describe "add question method succeeding" do
 	    it "adds question to a specific quiz" do
 	    	sign_in @instructor
-	    	@quiz = create(:quiz)
-	    	@group = create(:group)
+	    	assign_create_quiz_group
 	    	@group.students << @student
-	    	@instructor.quizzes << @quiz
-	    	@instructor.groups << @group
 	    	post :add_question, { quiz_id: @quiz.id, question: {text: "Example Question?", mark: 2, right_answer: "a", choices: ["a","b","c","d"]}}
 	    	quiz_response = json(response.body)
 	    	expect(response.status).to eq(201)
@@ -209,15 +194,37 @@ RSpec.describe Api::QuizzesController, :type => :controller do
     describe "add question method failing" do
 	    it "fails to add question to a specific quiz due to validations failure" do
 	    	sign_in @instructor
-	    	@quiz = create(:quiz)
-	    	@group = create(:group)
+	    	assign_create_quiz_group	
 	    	@group.students << @student
-	    	@instructor.quizzes << @quiz
-	    	@instructor.groups << @group
 	    	post :add_question, { quiz_id: @quiz.id, question: {text: "", mark: 2, right_answer: "a", choices: ["a","b","c","d"]}}
 	    	quiz_response = json(response.body)
 	    	expect(response.status).to eq(422)
 	    	expect(quiz_response[:info][:text]).to eq(["can't be blank"])
 	    end
     end
+
+    describe "edit question method succeeding" do
+	    it "change any of the attributes of the question " do
+	    	sign_in @instructor
+	    	assign_create_quiz_group
+	    	assign_create_question
+	    	patch :edit_question, { question_id: @question.id, question: {choices: ["abcd","efgh","hijk","lmnop"]}}
+	    	quiz_response = json(response.body)
+	    	expect(response.status).to eq(200)
+	    	expect(quiz_response[:data][:question][:choices]).to eql(["abcd","efgh","hijk","lmnop"])
+	    	expect(quiz_response[:data][:question][:right_answer]).to eql("a")
+	    end
+    end
+end
+
+def assign_create_quiz_group
+	@quiz = create(:quiz)
+	@group = create(:group)
+	@instructor.quizzes << @quiz
+	@instructor.groups << @group
+end
+
+def assign_create_question
+	@question = create(:question)
+	@quiz.questions << @question
 end
