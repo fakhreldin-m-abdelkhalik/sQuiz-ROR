3bf271c002d086fcfb3df299cbd2d31f69d1dfdc
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2015-02-12 15:05:25 +0200

    Issue #15,Change delete group to be post

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
     post 'groups/student/remove' => 'groups#remove'
     post 'groups/create'=>'groups#create'
     post 'student/mark_quiz', to: 'quizzes#mark_quiz'
-    delete 'groups/delete'=>'groups#destroy'
+    post 'groups/delete'=>'groups#destroy'
   end
 
 end
