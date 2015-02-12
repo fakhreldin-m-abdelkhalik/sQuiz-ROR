623811efff1b3c6b634cc80e3e9ffd4119dfe9a7
AhmedFathiShaban <ahmed.1331@yahoo.com>
2015-02-12 16:28:42 +0200

    issue #20 , added some factories

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -9,4 +9,14 @@ FactoryGirl.define do
     email 'abcdefg@eng.asu.edu.eg' 
     password 'abc12345678'
   end
+  factory :student2 ,class: Student do
+    name'student2' 
+    email 'abcdssefg@eng.asu.edu.eg' 
+    password 'abc1ss2345678'
+  end
+  factory :student3 ,class: Student do
+    name'student3' 
+    email 'abcdefssasdxg@eng.asu.edu.eg' 
+    password 'abc12as345678'
+  end
 end
