fba952756cf2a4faaaee5ae4340bc263c52da01d
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2015-02-13 15:09:09 +0200

    Issue #15,Add routes for create quiz

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -46,7 +46,7 @@ module Api
 			quiz = Quiz.new(quiz_params)
 			if quiz.save
 				current_instructor.quizzes << quiz
-				render json: { success: true, data:{:quiz => quiz}, info:{} }, status: 201
+				render json: { id: quiz.id }, status: 201
 			else
 				render json: { error: quiz.errors }, status: 422
 			end
@@ -100,7 +100,7 @@ module Api
 				quiz = current_instructor.quizzes.find(params[:quiz_id])
 				no = quiz.no_of_MCQ + quiz.no_of_rearrangeQ	
 				no.times do |n|
-					question = Question.create((params["_json"][n]).permit([:text, :mark, :right_answer, :choices => [],]))
+					question = Question.create((params["_json"][n]).permit([:text, :mark, :right_answer, :choices => []]))
 					quiz.questions << question
 				end
 				render json: { info: "created"}, status: 201
