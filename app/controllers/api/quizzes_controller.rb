c295ce572bbd7ff3ac1fa3a682abe69516b2608c
fakhreldin-m-abdelkhalik <fakhreldin.m.abdelkhalik@gmail.com>
2015-02-13 21:39:43 +0200

    Issue #15, Adjust order for questions at index

--- app/controllers/api/quizzes_controller.rb
+++ app/controllers/api/quizzes_controller.rb
@@ -34,7 +34,7 @@ module Api
 		def instructor_show
 			if (current_instructor.quizzes.exists?(:id => params[:id]))
 				quiz = current_instructor.quizzes.find(params[:id])
-				questions = quiz.questions
+				questions = quiz.questions.order(created_at: :desc)
 				render json: questions, status: 200
 			else
 				render json: { error:"Quiz is not found" }, status: 404
